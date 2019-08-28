726bceccef3316e0157fc15d9c337957ccf3a79f
discussion4all <discussion4all@yahoo.com>
2019-08-28 16:09:51 +0530

    created functionality on P Test Page where charts & graphs can be swapped

--- src/views/Pages/pTestPage.jsx
+++ src/views/Pages/pTestPage.jsx
@@ -18,6 +18,7 @@ import CardHeader from "components/Card/CardHeader.jsx";
 import CardIcon from "components/Card/CardIcon.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 import CardFooter from "components/Card/CardFooter.jsx";
+import "./custom.css";
 
 import {
   roundedLineChart,
@@ -36,10 +37,8 @@ import { WidthProvider, Responsive } from "react-grid-layout";
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 const originalLayouts = getFromLS("layouts") || {};
 
-
 class pTestPage extends React.Component {
-    
-    constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -50,7 +49,7 @@ class pTestPage extends React.Component {
   static get defaultProps() {
     return {
       className: "",
-      cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+      cols: { lg: 16, md: 12, sm: 6, xs: 4, xxs: 2 },
       rowHeight: 30
     };
   }
@@ -64,38 +63,37 @@ class pTestPage extends React.Component {
     saveToLS("layouts", layouts);
     this.setState({ layouts });
   }
-    render() {
+  render() {
     const { classes } = this.props;
     return (
       <div>
         <Heading
           textAlign="center"
-          title="React Chartist"
+          title="React Chartist with swapable functionality"
           category={
             <span>
               A react wrapper for{" "}
               <a
                 target="_blank"
                 href="https://gionkunz.github.io/chartist-js/"
-                rel="noopener noreferrer"
-              >
+                rel="noopener noreferrer">
                 Chartist.js
-              </a>. Please checkout the{" "}
+              </a>
+              . Please checkout the{" "}
               <a
                 href="https://gionkunz.github.io/chartist-js/getting-started.html"
                 target="_blank"
-                rel="noopener noreferrer"
-              >
+                rel="noopener noreferrer">
                 full documentation of Chartist.js
               </a>{" "}
               and{" "}
               <a
                 href="https://fraserxu.me/react-chartist/"
                 target="_blank"
-                rel="noopener noreferrer"
-              >
+                rel="noopener noreferrer">
                 full documentation of react-chartist
-              </a>.
+              </a>
+              .
             </span>
           }
         />
@@ -104,14 +102,11 @@ class pTestPage extends React.Component {
           {...this.props}
           layouts={this.state.layouts}
           onLayoutChange={(layout, layouts) => this.onLayoutChange(layout, layouts)}>
-          <div
-            key="1"
-            data-grid={{ w: 3.5, h: 8, x: 0, y: 0, minW: 2, minH: 8 }}
-            style={{ border: "1px solid purple" }}>
-            <GridItem xs={12} sm={12} md={10}>
-            <Card chart>
-              <CardHeader color="rose">
+          <div key="1" data-grid={{ w: 4, h: 8.7, x: 0, y: 0, minW: 2 }}>
+            <Card chart style={{ height: "100%" }}>
+              <CardHeader color="rose" style={{ height: "80%" }}>
                 <ChartistGraph
+                  style={{ height: "100%" }}
                   className="ct-chart-white-colors"
                   data={roundedLineChart.data}
                   type="Line"
@@ -119,21 +114,17 @@ class pTestPage extends React.Component {
                   listener={roundedLineChart.animation}
                 />
               </CardHeader>
-              <CardBody>
+              <CardBody style={{ height: "20%" }}>
                 <h4 className={classes.cardTitle}>Rounded Line Chart</h4>
                 <p className={classes.cardCategory}>Line Chart</p>
               </CardBody>
             </Card>
-          </GridItem>
           </div>
-          <div
-            key="2"
-            data-grid={{ w: 4, h: 8, x: 2, y: 0, minW: 2, minH: 8 }}
-            style={{ border: "1px solid purple" }}>
-            <GridItem xs={12} sm={12} md={10}>
-            <Card chart>
-              <CardHeader color="warning">
+          <div key="2" data-grid={{ w: 4, h: 8.7, x: 4, y: 0, minW: 2 }}>
+            <Card chart style={{ height: "100%" }}>
+              <CardHeader color="warning" style={{ height: "80%" }}>
                 <ChartistGraph
+                  style={{ height: "100%" }}
                   className="ct-chart-white-colors"
                   data={straightLinesChart.data}
                   type="Line"
@@ -141,24 +132,17 @@ class pTestPage extends React.Component {
                   listener={straightLinesChart.animation}
                 />
               </CardHeader>
-              <CardBody>
+              <CardBody style={{ height: "20%" }}>
                 <h4 className={classes.cardTitle}>Straight Lines Chart</h4>
                 <p className={classes.cardCategory}>Line Chart with Points</p>
               </CardBody>
             </Card>
-          </GridItem>
-
-
-            
           </div>
-          <div
-            key="3"
-            data-grid={{ w: 4, h: 8, x: 4, y: 0, minW: 2, minH: 8 }}
-            style={{ border: "1px solid purple" }}>
-             <GridItem xs={12} sm={12} md={10}>
-            <Card chart>
-              <CardHeader color="info">
+          <div key="3" data-grid={{ w: 4, h: 8.7, x: 8, y: 0, minW: 2, minH: 3 }}>
+            <Card chart style={{ height: "100%" }}>
+              <CardHeader color="info" style={{ height: "80%" }}>
                 <ChartistGraph
+                  style={{ height: "100%" }}
                   className="ct-chart-white-colors"
                   data={simpleBarChart.data}
                   type="Bar"
@@ -167,84 +151,15 @@ class pTestPage extends React.Component {
                   listener={simpleBarChart.animation}
                 />
               </CardHeader>
-              <CardBody>
+              <CardBody style={{ height: "20%" }}>
                 <h4 className={classes.cardTitle}>Simple Bar Chart</h4>
                 <p className={classes.cardCategory}>Bar Chart</p>
               </CardBody>
             </Card>
-          </GridItem>
           </div>
-          <div
-            key="4"
-            data-grid={{ w: 2, h: 3, x: 6, y: 0, minW: 2, minH: 3 }}
-            style={{ border: "1px solid purple" }}>
-            <span className="text">4</span>
-          </div>
-          <div
-            key="5"
-            data-grid={{ w: 2, h: 3, x: 8, y: 0, minW: 2, minH: 3 }}
-            style={{ border: "1px solid purple" }}>
-            <span className="text">5</span>
-          </div>
-        </ResponsiveReactGridLayout>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={4}>
-            <Card chart>
-              <CardHeader color="rose">
-                <ChartistGraph
-                  className="ct-chart-white-colors"
-                  data={roundedLineChart.data}
-                  type="Line"
-                  options={roundedLineChart.options}
-                  listener={roundedLineChart.animation}
-                />
-              </CardHeader>
-              <CardBody>
-                <h4 className={classes.cardTitle}>Rounded Line Chart</h4>
-                <p className={classes.cardCategory}>Line Chart</p>
-              </CardBody>
-            </Card>
-          </GridItem>
-          <GridItem xs={12} sm={12} md={4}>
-            <Card chart>
-              <CardHeader color="warning">
-                <ChartistGraph
-                  className="ct-chart-white-colors"
-                  data={straightLinesChart.data}
-                  type="Line"
-                  options={straightLinesChart.options}
-                  listener={straightLinesChart.animation}
-                />
-              </CardHeader>
-              <CardBody>
-                <h4 className={classes.cardTitle}>Straight Lines Chart</h4>
-                <p className={classes.cardCategory}>Line Chart with Points</p>
-              </CardBody>
-            </Card>
-          </GridItem>
-          <GridItem xs={12} sm={12} md={4}>
-            <Card chart>
-              <CardHeader color="info">
-                <ChartistGraph
-                  className="ct-chart-white-colors"
-                  data={simpleBarChart.data}
-                  type="Bar"
-                  options={simpleBarChart.options}
-                  responsiveOptions={simpleBarChart.responsiveOptions}
-                  listener={simpleBarChart.animation}
-                />
-              </CardHeader>
-              <CardBody>
-                <h4 className={classes.cardTitle}>Simple Bar Chart</h4>
-                <p className={classes.cardCategory}>Bar Chart</p>
-              </CardBody>
-            </Card>
-          </GridItem>
-        </GridContainer>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={6}>
-            <Card>
-              <CardHeader color="info" icon>
+          <div key="4" data-grid={{ w: 6, h: 11, x: 0, y: 2, minW: 2, minH: 3 }}>
+            <Card style={{ height: "100%" }}>
+              <CardHeader color="info" icon style={{ height: "20%" }}>
                 <CardIcon color="info">
                   <Timeline />
                 </CardIcon>
@@ -252,8 +167,9 @@ class pTestPage extends React.Component {
                   Coloured Line Chart <small>- Rounded</small>
                 </h4>
               </CardHeader>
-              <CardBody>
+              <CardBody style={{ height: "80%" }}>
                 <ChartistGraph
+                  style={{ height: "100%" }}
                   data={colouredLineChart.data}
                   type="Line"
                   options={colouredLineChart.options}
@@ -261,10 +177,34 @@ class pTestPage extends React.Component {
                 />
               </CardBody>
             </Card>
-          </GridItem>
-          <GridItem xs={12} sm={12} md={6}>
-            <Card>
-              <CardHeader color="rose" icon>
+          </div>
+          <div key="5" data-grid={{ w: 6, h: 11, x: 6, y: 2, minW: 2, minH: 3 }}>
+            <Card style={{ height: "100%" }}>
+              <CardHeader color="danger" icon style={{ height: "20%" }}>
+                <CardIcon color="danger">
+                  <Timeline />
+                </CardIcon>
+                <h4 className={classes.cardIconTitle}>Pie Chart</h4>
+              </CardHeader>
+              <CardBody style={{ height: "80%" }}>
+                <ChartistGraph
+                  data={pieChart.data}
+                  type="Pie"
+                  options={pieChart.options}
+                  style={{ height: "100%" }}
+                />
+              </CardBody>
+              <CardFooter stats className={classes.cardFooter}>
+                <h6 className={classes.legendTitle}>Legend</h6>
+                <i className={"fas fa-circle " + classes.info} /> Apple{` `}
+                <i className={"fas fa-circle " + classes.warning} /> Samsung{` `}
+                <i className={"fas fa-circle " + classes.danger} /> Windows Phone{` `}
+              </CardFooter>
+            </Card>
+          </div>
+          <div key="6" data-grid={{ w: 6, h: 11, x: 0, y: 4, minW: 2, minH: 3 }}>
+            <Card style={{ height: "100%" }}>
+              <CardHeader color="rose" icon style={{ height: "20%" }}>
                 <CardIcon color="rose">
                   <Timeline />
                 </CardIcon>
@@ -272,8 +212,9 @@ class pTestPage extends React.Component {
                   Multiple Bars Chart <small>- Bar Chart</small>
                 </h4>
               </CardHeader>
-              <CardBody>
+              <CardBody style={{ height: "80%" }}>
                 <ChartistGraph
+                  style={{ height: "100%" }}
                   data={multipleBarsChart.data}
                   type="Bar"
                   options={multipleBarsChart.options}
@@ -281,12 +222,10 @@ class pTestPage extends React.Component {
                 />
               </CardBody>
             </Card>
-          </GridItem>
-        </GridContainer>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={7}>
-            <Card>
-              <CardHeader color="warning" icon>
+          </div>
+          <div key="7" data-grid={{ w: 6, h: 11, x: 6, y: 4, minW: 2, minH: 3 }}>
+            <Card style={{ height: "100%" }}>
+              <CardHeader color="warning" icon style={{ height: "20%" }}>
                 <CardIcon color="warning">
                   <Timeline />
                 </CardIcon>
@@ -294,8 +233,9 @@ class pTestPage extends React.Component {
                   Coloured Lines Chart <small>- Rounded</small>
                 </h4>
               </CardHeader>
-              <CardBody>
+              <CardBody style={{ height: "20%" }}>
                 <ChartistGraph
+                  style={{ height: "100%" }}
                   data={colouredLinesChart.data}
                   type="Line"
                   options={colouredLinesChart.options}
@@ -303,41 +243,14 @@ class pTestPage extends React.Component {
                 />
               </CardBody>
             </Card>
-          </GridItem>
-          <GridItem xs={12} sm={12} md={5}>
-            <Card>
-              <CardHeader color="danger" icon>
-                <CardIcon color="danger">
-                  <Timeline />
-                </CardIcon>
-                <h4 className={classes.cardIconTitle}>Pie Chart</h4>
-              </CardHeader>
-              <CardBody>
-                <ChartistGraph
-                  data={pieChart.data}
-                  type="Pie"
-                  options={pieChart.options}
-                />
-              </CardBody>
-              <CardFooter stats className={classes.cardFooter}>
-                <h6 className={classes.legendTitle}>Legend</h6>
-                <i className={"fas fa-circle " + classes.info} /> Apple{` `}
-                <i
-                  className={"fas fa-circle " + classes.warning}
-                /> Samsung{` `}
-                <i className={"fas fa-circle " + classes.danger} /> Windows
-                Phone{` `}
-              </CardFooter>
-            </Card>
-          </GridItem>
-        </GridContainer>
+          </div>
+        </ResponsiveReactGridLayout>
       </div>
     );
   }
 }
 
 export default withStyles(chartsStyle)(pTestPage);
-
 
 function getFromLS(key) {
   let ls = {};
@@ -361,4 +274,3 @@ function saveToLS(key, value) {
     );
   }
 }
-
